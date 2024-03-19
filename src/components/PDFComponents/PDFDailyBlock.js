312cf9f2c05dff8bdc01255d4de2ca9e32c6b922
Johnny <ion.tugui.cs@gmail.com>
2024-03-19 22:11:30 +0000

    Update PDFDailyBlock.js

--- src/components/PDFComponents/PDFDailyBlock.js
+++ src/components/PDFComponents/PDFDailyBlock.js
@@ -2,14 +2,16 @@ import React from "react";
 import { Text, View,Font, StyleSheet, Image} from "@react-pdf/renderer";
 import DailyBlock from "../forecast/DailyBlock";
 
+
+
 const styles = StyleSheet.create({
     /* Styling the box containing the days */
     Block:{ 
         textAlign: 'center',
         display: 'inline-block', 
-        width: '15vw', 
+        width: '25vw', 
         marginTop: 20,
-        height: '15vw',
+        height: '17vw',
         backgroundColor: '#333333', 
         fontSize: '2vw',
         borderTopLeftRadius: '30%',
@@ -18,7 +20,8 @@ const styles = StyleSheet.create({
         borderBottomLeftRadius: '30%',
         marginRight: 10, 
         marginBottom: 10,
-        padding: 10,
+        paddingVerical: 10,
+        paddingHorizontal: 1,
     },
 
     /*Inner parent styling */
@@ -37,7 +40,15 @@ const styles = StyleSheet.create({
 
     /*Icon styling */
     icon:{
+        justifyContent:'center',
         width: '50%'
+    },
+
+    /*Text Styling*/ 
+    Text:{
+        paddingTop: 5,
+        fontFamily:'Roboto',
+        color:'white',
     }
 
 })
@@ -46,10 +57,10 @@ const PDFDailyBlock = ({day,icon, summary}) => {
     return (
     <View style={styles.Block}>
         <View style={styles.InnerParrent}>
-            <Text>{day}</Text>
+            <Text style={styles.Text}>{day}</Text>
         <View style={styles.IconAndTemp}> 
             <Image alt="icon" style={styles.icon} src={process.env.PUBLIC_URL + `/icons/${icon}.png`} onError = {DailyBlock.handleImageError}/>
-            <Text>{summary}°C</Text>
+            <Text style={styles.Text}>{summary}°C</Text>
         </View>
         </View>
     </View>
